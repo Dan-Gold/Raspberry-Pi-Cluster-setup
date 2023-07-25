b0aeb728242b9c04563b77e018a1260996d3bb99
Dan-Gold <dan.goldschmidt96@gmail.com>
2023-07-24 23:13:11 -0400

    updates to head setup

--- cluster-head-setup.sh
+++ cluster-head-setup.sh
@@ -4,10 +4,17 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+# Install tools
+sudo apt-get install nmap -y
+sudo apt-get install sshpass -y
+sudo apt-get install vim -y
+sudo apt autoremove -y
+
 # Change the hostname to "rpi0"
 sudo hostnamectl set-hostname rpi0
 
 # Print the new hostname
 echo "Hostname has been changed to: $(hostname)"
 
+echo "This pi will reboot, next run 'node-ssh-setup.sh' which will configure all of the other raspberry pi nodes"
 sudo reboot
